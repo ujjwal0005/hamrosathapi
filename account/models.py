d8d437a9b9460e080dfaac5d27c3d8df7952c2d1
ujjwal sapkota <ujjwalsapkota005@gmail.com>
2022-08-14 10:24:33 +0545

    changes

--- account/models.py
+++ account/models.py
@@ -35,7 +35,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     objects = CustomAccountManager()
 
     def __str__(self):
-        return self.email
+        return f'{self.email}'
 
 class DoctorProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='doctor_profile',primary_key=True)
