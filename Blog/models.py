086c66d108d1533a94731bbe3dd1311480e86c1b
ujjwal sapkota <ujjwalsapkota005@gmail.com>
2022-08-16 12:50:19 +0545

    Updated blog model

--- Blog/models.py
+++ Blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog')
-    image = models.ImageField(upload_to='media',null=True,blank=True)
+    image = models.ImageField(upload_to='blogs',null=True,blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     is_verified = models.BooleanField(default=True)
